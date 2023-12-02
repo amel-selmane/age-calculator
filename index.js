3bdd1ec6aa99d78b7bd518fc82d51227d5c9e7d3
amel-selmane <selmane.amel@gmail.com>
2023-12-02 16:31:24 +0100

    Style almost finished

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// Get input elements
+const dayInput = document.querySelector('#day');
+const monthInput = document.querySelector('#month');
+const yearInput = document.querySelector('#year');
+
+// Get result elements
+const years = document.querySelector('.years span');
+const months = document.querySelector('.months span');
+const days = document.querySelector('.days span');
+
 document.querySelector('.validator button').addEventListener('click', e => {
     e.preventDefault();
+
+    
 })
